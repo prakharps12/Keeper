7ccc8efe528cd69bb17a6d796ada2441ab7f1681
Prakhar Pratap Singh <98352295+prakharps12@users.noreply.github.com>
2024-06-17 00:29:59 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the note's Title</h1>
+      <p>This is the actual Note.</p>
+    </div>
+  );
+}
+
+export default Note;
