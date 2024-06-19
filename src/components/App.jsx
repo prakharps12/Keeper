dd43cea8b237e1c7d75298bc063aea689520d666
Prakhar Pratap Singh <98352295+prakharps12@users.noreply.github.com>
2024-06-19 18:57:30 +0000

    adding notes.js to keeper

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
       <Heading />
-      <Note />
+      {notes.map((note) => {
+        return <Note title={note.title} content={note.content} />;
+      })}
+      {/* <Note title="present title " content="present content" /> */}
       <Footer />
     </div>
   );
